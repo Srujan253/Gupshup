918f2395a2d70416c9f3424933ce129635919849
Srujan253 <142961509+Srujan253@users.noreply.github.com>
2025-07-18 13:43:54 +0530

    Error in deploment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,10 +23,13 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production"
+      ? "https://gupshup-rbcp.onrender.com"
+      : "http://localhost:5173",
     credentials: true,
   })
 );
+
 
 // Routes
 app.use("/api/auth", authRoutes);
