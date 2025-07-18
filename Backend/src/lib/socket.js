60e39de909886ee362de07e76a09f81bb6a9e2c9
Srujan253 <142961509+Srujan253@users.noreply.github.com>
2025-07-18 14:24:27 +0530

    socket fixed

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -3,11 +3,12 @@ import { Server } from "socket.io";
 
 let io;
 const userSocketMap = {}; // userId -> socket.id
-
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173"],
+      origin: process.env.NODE_ENV === "production"
+        ? "https://gupshup-rbcp.onrender.com"
+        : "http://localhost:5173",
       credentials: true,
     },
   });
